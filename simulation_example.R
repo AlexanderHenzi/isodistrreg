89555b442427ceeb005732004dd564fde646007c
AlexanderHenzi <alexander.henzi@stat.unibe.ch>
2020-11-13 07:47:03 +0100

    functions for replicating simulation example

---
+++ simulation_example.R
@@ -0,0 +1,201 @@
+#------------------------------------------------------------------------------#
+# This code was used to compute the simulation examples in 
+
+# Alexander Henzi, Johanna F. Ziegel, and Tilmann Gneiting. Isotonic
+# Distributional Regression. arXiv e-prints, art. arXiv:1909.03725, Sep 2019.
+# URL https://arxiv.org/abs/1909.03725.
+
+# File is available on GitHub: https://github.com/AlexanderHenzi/isodistrreg.
+
+# Computation may take a few minutes depending on your CPU. Reduce "n" (size of
+# training data) and "nValid" (size of validation data) to increase the speed.
+
+# Last revised 2020/11/04
+
+#------------------------------------------------------------------------------#
+# packages (must be installed)
+library(tidyverse)
+library(scoringRules)
+library(isodistrreg)
+library(np)
+library(tram)
+library(quantreg)
+library(splines)
+library(grf)
+
+#------------------------------------------------------------------------------#
+# functions (must be contained in working directory)
+source("simulation_example_functions.R")
+
+#------------------------------------------------------------------------------#
+# setup
+
+# select type of simulation example:
+# - "S" for "smooth"
+# - "D" for "discontinuous"
+# - "P" for "poisson"
+# - "NI" for "non-isotonic"
+type <- "S" 
+
+# sample size for training and validation data (reduce to increase speed)
+n <- 500
+nValid <- 5000
+
+#------------------------------------------------------------------------------#
+# data generation
+nFit <- n
+N <- nFit + nValid
+
+# id <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID")) # only on HPC cluster
+# id runs from 1 to 500
+# set.seed(n + id) # for reproducibility of training data
+
+x <- runif(nFit, 0, 10)
+y <- switch(type,
+  "D" = rgamma(nFit, shape = sqrt(x), scale = pmin(pmax(x, 1), 6))
+          + 10 * (x >= 5),
+  "P" = rpois(nFit, pmin(pmax(x, 1), 6)),
+  "NI" = rgamma(nFit, shape = sqrt(x), scale = pmin(pmax(x, 1), 6))
+          - 2 *(x > 7),
+  "S" = rgamma(nFit, shape = sqrt(x), scale = pmin(pmax(x, 1), 6)),
+)
+
+# set.seed(id) # for reproducibility of validation data
+
+xValid <- runif(nValid, 0, 10)
+yValid <- switch(type,
+  "D" = rgamma(
+    nValid,
+    shape = sqrt(xValid),
+    scale = pmin(pmax(xValid, 1), 6)
+  ) + 10 * (xValid >= 5),
+  "P" = rpois(nValid, pmin(pmax(xValid, 1), 6)),
+  "NI" = rgamma(nValid, shape = sqrt(xValid), scale = pmin(pmax(xValid, 1), 6))
+          - 2 *(xValid > 7),
+  "S" = rgamma(nValid, shape = sqrt(xValid), scale = pmin(pmax(xValid, 1), 6))
+)
+x <- c(x, xValid)
+y <- c(y, yValid)
+data <- data.frame(y = y, x = x)
+
+# data frame for distributional regression methods using splines
+splineData <- cbind(
+  y = y,
+  as.data.frame(bs(x, knots = 2 * (1:4), Boundary.knots = c(0, 10)))
+)
+colnames(splineData) <- c("y", paste0("x", seq_len(ncol(splineData) - 1)))
+
+# training/validation split
+newdata <- data[(nFit + 1):N, "x", drop = FALSE]
+newSpData <- splineData[(nFit + 1):N, -1]
+newY <- y[(nFit + 1):N]
+data <- data[1:nFit, ]
+splineData <- splineData[1:nFit, ]
+
+#------------------------------------------------------------------------------#
+# model fits, prediction, crps
+method <- c("idr", "idrbag", "np", "sqr", "tram", "qrf")
+crpsRes <- seq_along(method)
+names(crpsRes) <- method
+
+dataList <- list(
+  # training data for each method
+  data,
+  data,
+  switch(type,"P" = mutate(data, y = factor(y, ordered = TRUE)), data),
+  splineData,
+  switch(
+    type,
+    "P" = mutate(splineData, y = factor(y, ordered = TRUE)),
+    splineData
+  ),
+  data
+)
+MoreArgs <- list(
+    # arguments for distributional regression methods
+      # idr
+      # idrbag
+      # np
+  bwtype = "adaptive_nn",
+  nmulti = 4,
+  oykertype = "liracine",
+      # tram
+      # rq
+  tau = seq(0.005, 0.995, 0.001),
+      # qrf
+  min.node.size = 40,
+  sample.fraction = 0.5,
+  honesty.fraction = 0.5,
+  honesty = TRUE,
+  quantiles = seq(0.01, 0.99, 0.01)
+)
+MoreArgsPred <- list(
+  # arguments for predict methods
+    digits = 6,                                   # round predictive cdfs
+    b = 100,                                      # number of bagging samples
+    y = y,                                        # for idr bagging
+    K = 5000,                                     # grid for cdfs (tram)
+    grd = switch(type,
+      "P" = factor(0:max(y), ordered = TRUE),    # grid for np predictions
+      seq(min(newY) - 5, max(newY) + 5, 0.1)
+    ),
+    tau = seq(0.005, 0.995, 0.001)               # for qrf
+)
+newdataList <- list(
+  # data for out of sample predictions
+  newdata,
+  newdata,
+  newdata,
+  newSpData,
+  newSpData,
+  newdata
+)
+
+#------------------------------------------------------------------------------#
+# run estimation, prediction, compute crps
+for (j in seq_along(method)) {
+  fit <- do.call(
+    what = probFit,
+    args = c(list(method = method[[j]], data = dataList[[j]]), MoreArgs)
+  )
+  pred <- do.call(
+    what = probPredict,
+    c(list(fit = fit, newdata = newdataList[[j]]), MoreArgsPred)
+  )
+  if (type == "P" & method[j] == "np") {
+    pred <- structure(
+      map(pred, ~mutate(., points = as.numeric(as.character(points)))),
+      class = c("idr", "nppred")
+    )
+  }
+  crpsRes[j] <- mean(crps(pred, newY))
+}
+
+# add ideal forecast
+crpsRes <- c(
+  crpsRes,
+  ideal = switch(type,
+    "P" = mean(scoringRules::crps_pois(
+      y = newY,
+      lambda = pmin(pmax(newdata$x, 1), 6)
+    )),
+    "D" = mean(scoringRules::crps_gamma(
+      y = newY - 10 * (newdata$x >= 5),
+      shape = sqrt(newdata$x),
+      scale = pmin(pmax(newdata$x, 1), 6)
+    )),
+    "S" = mean(scoringRules::crps_gamma(
+      y = newY,
+      shape = sqrt(newdata$x),
+      scale = pmin(pmax(newdata$x, 1), 6)
+    )),
+    "NI" = mean(scoringRules::crps_gamma(
+      y = newY + 2 *(newdata$x > 7),
+      shape = sqrt(newdata$x),
+      scale = pmin(pmax(newdata$x, 1), 6)
+    ))
+  )
+)
+
+# print results
+crpsRes
---
+++ simulation_example_functions.R
@@ -0,0 +1,212 @@
+#------------------------------------------------------------------------------#
+# These functions are used in "simulation_example.R" for the computation of
+# distributional regression models in the simulation study of
+
+# Alexander Henzi, Johanna F. Ziegel, and Tilmann Gneiting. Isotonic
+# Distributional Regression. arXiv e-prints, art. arXiv:1909.03725, Sep 2019.
+# URL https://arxiv.org/abs/1909.03725.
+
+# File is available on GitHub: https://github.com/AlexanderHenzi/isodistrreg.
+
+# Last revised 2020/11/04
+
+#------------------------------------------------------------------------------#
+# fit distributional regression models
+
+probFit <- function(method, data, ...) {
+  # check data format
+  if (!is.data.frame(data) || !identical("y", colnames(data)[1]))
+    stop("Check input data!")
+  
+  # dot-arguments
+  dotArgs <- list(...)
+  
+  # regerssion methods
+  if (identical(method, "idr")) {
+    y <- data[, 1]
+    X <- data[, -1, drop = FALSE]
+    idr(y = y, X = X)
+  } else if (identical(method, "idrbag")) {
+    data[, -1, drop = FALSE]
+  } else if (identical(method, "np")) {
+    ydat <- data$y
+    xdat <- data[, -1, drop = FALSE]
+    npcdistbw(
+      ydat = ydat,
+      xdat = xdat,
+      oykertype = dotArgs$oykertype,
+      nmulti = dotArgs$nmulti
+    )
+  } else if (identical(method, "tram")) {
+    if (is.ordered(data$y)) {
+      Polr(
+        y ~ .,
+        data = data        
+      )
+    } else {
+      Colr(
+        y ~ .,
+        data = data
+      )
+    }
+  } else if (identical(method, "sqr")) {
+    rq(
+      y ~ .,
+      data = data,
+      tau = dotArgs$tau
+    )
+  } else if (identical(method, "qrf")) {
+    quantile_forest(
+      X = matrix(data$x, ncol = 1),
+      Y = data$y,
+      quantiles = dotArgs$quantiles,
+      min.node.size = dotArgs$min.node.size,
+      sample.fraction = dotArgs$sample.fraction,
+      honesty.fraction = dotArgs$honesty.fraction,
+      honesty = dotArgs$honesty
+    )
+  }
+}
+
+#------------------------------------------------------------------------------#
+# get predictions from distributional regression model
+probPredict <- function(fit, newdata, ...) {
+  UseMethod("probPredict")
+}
+
+# method for idr
+probPredict.idrfit <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+  digits <- dotArgs$digits
+
+  # get predictions
+  predict(
+    object = fit,
+    data = newdata,
+    digits = digits,
+    interpolation = "linear"
+  )
+}
+
+# method for idr bagging (directly from data.frame)
+probPredict.data.frame <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+  y <- dotArgs$y
+  digits <- dotArgs$digits
+  b <- dotArgs$b
+  
+  # get predictions
+  interpolation <- "linear"
+  X <- fit
+  grd <- sort(unique(y))
+  N <- nrow(X)
+  preds <- matrix(nrow = nrow(newdata), ncol = length(grd), 0)
+  for (i in seq_len(b)) {
+    sel <- sample(N, ceiling(N / 2))
+    fit <- idr(y = y[sel], X = X[sel, , drop = FALSE])
+    prd <- predict(fit, newdata, digits = digits, interpolation = interpolation)
+    preds <- preds + cdf(prd, grd)
+  }
+  preds <- preds / b
+  
+  # output
+  cdf <- lapply(
+    asplit(round(preds, digits), 1),
+    function(x) data.frame(points = grd, cdf = x)
+  )
+  cdf <- lapply(
+    cdf,
+    function(x) x[c(x$cdf[1] > 0, x$cdf[-1] > x$cdf[-nrow(x)]), ]
+  )
+  structure(unname(cdf), class = c("idr", "idrbag"))
+}
+
+# method for np
+probPredict.condbandwidth <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+  grd <- dotArgs$grd
+  digits <- dotArgs$digits
+  
+  # get predictions
+  ngrd <- length(grd)
+  nnew <- nrow(newdata)
+  preds <- npcdist(
+    bws = fit,
+    exdat = newdata[rep(seq_len(nnew), each = ngrd), , drop = FALSE],
+    eydat = rep(grd, nnew)
+  )
+  
+  # output
+  probs <- split(round(preds$condist, digits), rep(seq_len(nnew), each = ngrd))
+  cdf <- lapply(probs, function(x) data.frame(points = grd, cdf = x))
+  cdf <- lapply(
+    cdf,
+    function(x) x[c(FALSE, x$cdf[-1] > x$cdf[-nrow(x)]), , drop = FALSE]
+  )
+  structure(unname(cdf), class = c("idr", "nppred"))
+}
+
+# method for tram
+probPredict.tram <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+  K <- dotArgs$K
+  digits <- dotArgs$digits
+  
+  # get predictions
+  preds <- predict(
+    object = fit,
+    newdata = newdata,
+    type = "distribution",
+    K = K
+  )
+  
+  # output
+  points <- as.numeric(rownames(preds))
+  probs <- asplit(round(preds, digits), 2)
+  cdf <- lapply(probs, function(x) data.frame(points = points, cdf = x))
+  cdf <- lapply(
+    cdf, 
+    function(x) x[c(x$cdf[1] > 0, x$cdf[-1] > x$cdf[-nrow(x)]), , drop = FALSE]
+  )
+  structure(unname(cdf), class = c("idr", "trampred"))
+}
+
+# method for quantreg
+probPredict.rqs <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+
+  # get predictions
+  preds <- t(apply(predict(fit, newdata = newdata), 1, sort))
+  
+  # output
+  qgrd <- fit$tau
+  qgrd[length(qgrd)] <- 1
+  cdf <- lapply(
+    asplit(preds, 1), 
+    function(x) data.frame(points = x, cdf = qgrd)
+  )
+  structure(unname(cdf), class = c("idr", "rqpred"))
+}
+
+# method for quantile random forest
+probPredict.quantile_forest <- function(fit, newdata, ...) {
+  # dot-arguments
+  dotArgs <- list(...)
+
+  # get predictions
+  qgrd <- dotArgs$tau
+  preds <- predict(fit, matrix(newdata$x, ncol = 1), quantiles = qgrd)
+  
+  # output
+  qgrd[length(qgrd)] <- 1
+  cdf <- lapply(
+    asplit(preds, 1), 
+    function(x) data.frame(points = x, cdf = qgrd)
+  )
+  structure(unname(cdf), class = c("idr", "rqpred"))  
+}
